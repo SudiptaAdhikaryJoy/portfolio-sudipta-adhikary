0900c6848b9f8dfce1022e43a398abd404dfc61d
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2023-10-27 23:46:46 +0600

    hero section update

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -14,7 +14,7 @@ const Computers = () => {
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
-        penumba={2}
+        penumba={3}
         intensity={2}
         castShadow
         shadow-mapSize={1024}
