865327d14f97a9ba61e30e5615b6dbcd3e1b09da
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2023-11-05 23:53:21 +0600

    intensity section edit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -14,7 +14,7 @@ const Computers = () => {
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
-        penumba={1}
+        penumba={2}
         intensity={2}
         castShadow
         shadow-mapSize={1024}
