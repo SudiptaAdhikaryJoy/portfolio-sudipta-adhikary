b24a3fc8276a2cf40ce5b9d61bd5257b6510b0f5
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2023-11-08 23:32:58 +0600

    fixed bug

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -14,7 +14,7 @@ const Computers = () => {
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
-        penumba={1}
+        penumba={3}
         intensity={1}
         castShadow
         shadow-mapSize={1024}
