d3fab3fd4460ab39423042ebb9cf4b1454730abb
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2023-11-09 23:39:47 +0600

    fix issues

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -15,7 +15,7 @@ const Computers = () => {
         position={[-20, 50, 10]}
         angle={0.12}
         penumba={3}
-        intensity={1}
+        intensity={2}
         castShadow
         shadow-mapSize={1024}
       />
