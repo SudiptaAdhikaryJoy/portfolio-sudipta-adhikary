4018fd3d7c6969d9172a253fe461903a1055fd2e
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2023-11-27 16:12:05 +0600

    about section updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../style";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+	return (
+		<>
+			<motion.dev variants={textVariant()}>
+				<p className={styles.sectionSubText}>Introduction</p>
+				<h2 className={styles.sectionHeadText}>Overview</h2>
+			</motion.dev>
+		</>
+	);
+};
 
-export default About
+export default About;
